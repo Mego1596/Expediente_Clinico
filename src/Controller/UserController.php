bdcfe7985e3dc7adc704196aff3d25e6329e15ae
Erick Palma <gokuso03@gmail.com>
2019-06-07 23:17:51 -0600

    inicio de correcion de mostrar de clinica y usuario

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -39,10 +39,10 @@ class UserController extends AbstractController
     {
         $em = $this->getDoctrine()->getManager();
         if($AuthUser->getUser()->getRol()->getNombreRol() == 'ROLE_SA'){
-            $RAW_QUERY = 'SELECT u.id as id,CONCAT(p.primer_nombre," ",IFNULL(p.segundo_nombre," ")," ",p.primer_apellido," ",IFNULL(p.segundo_apellido," ")) as nombre_completo,u.email as email, c.nombre_clinica as clinica, r.nombre_rol as rol, u.is_active as activo FROM `user` as u,rol as r,clinica as c, `persona` as p
+            $RAW_QUERY = 'SELECT u.id as id,CONCAT(p.primer_nombre," ",IFNULL(p.segundo_nombre," ")," ",p.primer_apellido," ",IFNULL(p.segundo_apellido," ")) as nombre_completo,u.email as email, c.nombre_clinica as clinica, r.descripcion as rol, u.is_active as activo FROM `user` as u,rol as r,clinica as c, `persona` as p
                 WHERE u.persona_id = p.id AND u.clinica_id = c.id AND u.rol_id = r.id AND r.nombre_rol != "ROLE_PACIENTE";';
         }else{
-            $RAW_QUERY = 'SELECT u.id as id,CONCAT(p.primer_nombre," ",IFNULL(p.segundo_nombre," ")," ",p.primer_apellido," ",IFNULL(p.segundo_apellido," ")) as nombre_completo,u.email as email, c.nombre_clinica as clinica, r.nombre_rol as rol, u.is_active as activo FROM `user` as u,rol as r,clinica as c, `persona` as p
+            $RAW_QUERY = 'SELECT u.id as id,CONCAT(p.primer_nombre," ",IFNULL(p.segundo_nombre," ")," ",p.primer_apellido," ",IFNULL(p.segundo_apellido," ")) as nombre_completo,u.email as email, c.nombre_clinica as clinica, r.descripcion as rol, u.is_active as activo FROM `user` as u,rol as r,clinica as c, `persona` as p
                 WHERE u.persona_id = p.id AND u.clinica_id = c.id AND u.rol_id = r.id AND r.nombre_rol != "ROLE_SA" AND r.nombre_rol != "ROLE_PACIENTE" AND c.id= '.$AuthUser->getUser()->getClinica()->getId().'
                 AND u.id !='.$AuthUser->getUser()->getId().';';
         }
@@ -225,6 +225,7 @@ class UserController extends AbstractController
             return $this->redirectToRoute('user_index');
         }
 
+      
         return $this->render('user/new.html.twig', [
             'user' => $user,
             'clinicas'   => $clinicas,
@@ -241,12 +242,26 @@ class UserController extends AbstractController
      */
     public function show(User $user, Security $AuthUser): Response
     {
+        $conn = $this->getDoctrine()->getManager()->getConnection();
+        $sql = '
+            SELECT CONCAT(p.primer_nombre," " ,IFNULL(p.segundo_nombre," ")," " ,p.primer_apellido," ",IFNULL(p.segundo_apellido," ")) as nombre_completo, r.descripcion as nombre_rol, c.nombre_clinica as nombre_clinica
+            FROM `user` as u, `persona` as p,  `rol` as r, `clinica` as c  WHERE
+            u.persona_id       =p.id            AND
+            u.rol_id           =r.id            AND
+            u.clinica_id       =c.id            AND
+            u.id = :idUsuario
+            ';
+        $stmt = $conn->prepare($sql);
+        $stmt->execute(array('idUsuario' => $user->getId()));
+        $result= $stmt->fetch();
         //VALIDACION DE BLOQUEO DE RUTAS EN CASO DE INTENTAR ACCEDER A REGISTROS DE OTRAS CLINICAS SI NO ES ROLE_SA
         if($AuthUser->getUser()->getRol()->getNombreRol() != 'ROLE_SA'){
             if($user->getRol()->getNombreRol() != 'ROLE_SA'){
                 if($AuthUser->getUser()->getClinica()->getId() == $user->getClinica()->getId()){
                     return $this->render('user/show.html.twig', [
                         'user' => $user,
+                        'persona'=>$result,
+
                     ]);
                 }else{
                     $this->addFlash('fail', 'Error, este registro puede que no exista o no le pertenece');
@@ -259,6 +274,7 @@ class UserController extends AbstractController
         }
         return $this->render('user/show.html.twig', [
             'user' => $user,
+            'persona'=>$result,
         ]);
     }
 
