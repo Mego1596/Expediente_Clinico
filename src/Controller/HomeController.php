4bf7f5b9e2791576422c1aff8995ef930ad50635
Mego1596 <maurigranados1596@gmail.com>
2019-06-12 10:47:41 -0600

    Update HomeController.php
    Reduccion de sentencia sql

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -79,7 +79,7 @@ class HomeController extends AbstractController
                         $this->addFlash('success', 'Contraseña modificada con éxito');
                         return $this->redirectToRoute('home');
                     }else{
-                        $this->addFlash('fail', 'Un problema ha ocurrido, la nueva contraseña debe coincidir con la confirmaciÓn de contraseña');
+                        $this->addFlash('fail', 'Un problema ha ocurrido, la nueva contraseña debe coincidir con la confirmacion de contraseña');
                     }
                 }else{
                     $this->addFlash('fail', 'Un problema ha ocurrido, compruebe que la contraseña actual sea correcta');
@@ -136,7 +136,7 @@ class HomeController extends AbstractController
             $result = $statement->fetchAll();
             return $this->json($result);
         }else{
-            $this->addFlash('fail','Error, este expediente no es válido');
+            $this->addFlash('fail','Error, este expediente no es valido');
             $this->redirectToRoute('expediente_index');
         }
         
@@ -171,7 +171,7 @@ class HomeController extends AbstractController
             $result = $statement->fetchAll();
             return $this->json($result);
         }else{
-            $this->addFlash('fail','Error, este expediente no es válido');
+            $this->addFlash('fail','Error, este expediente no es valido');
             $this->redirectToRoute('expediente_index');
         }
         
@@ -220,7 +220,7 @@ class HomeController extends AbstractController
                             }
                         }
                         else{
-                            $this->addFlash('fail','Error, el estado del ingreso no puede estar vacío');
+                            $this->addFlash('fail','Error, el estado del ingreso no puede estar vacio');
                             $this->redirectToRoute('expediente_index');
 
                         }
@@ -228,22 +228,22 @@ class HomeController extends AbstractController
 
 
                     }else{
-                        $this->addFlash('fail','Error, este expediente no es válido');
+                        $this->addFlash('fail','Error, este expediente no es valido');
                         $this->redirectToRoute('expediente_index');
                     }
 
                 }else{
-                    $this->addFlash('fail', 'Error, no se seleccionó ningún expediente');
+                    $this->addFlash('fail', 'Error, no se selecciono ningun expediente');
                     $this->redirectToRoute('expediente_index');
                 }
 
             }else{
-                $this->addFlash('fail', 'Error, no se seleccionó ninguna habitación');
+                $this->addFlash('fail', 'Error, no se selecciono ninguna habitacion');
                 $this->redirectToRoute('expediente_index');
             }
 
         }else{
-            $this->addFlash('fail', 'Error, no se seleccionó ninguna sala');
+            $this->addFlash('fail', 'Error, no se selecciono ninguna sala');
             $this->redirectToRoute('expediente_index');
         }     
 
@@ -403,79 +403,14 @@ class HomeController extends AbstractController
                             SELECT 23 as id,'23:30:00-00:00:00' AS intervalo, 0 as cantidad UNION
                              
                             SELECT HOUR(fecha_reservacion) as id ,
-                                (CASE
-                                    WHEN TIME(fecha_reservacion) BETWEEN '00:00:00' AND '00:29:00' THEN '00:00:00-00:30:00'
-                                    WHEN TIME(fecha_reservacion) BETWEEN '00:30:00' AND '00:59:00' THEN '00:30:00-01:00:00'
-                                 
-                                    WHEN TIME(fecha_reservacion) BETWEEN '01:00:00' AND '01:29:00' THEN '01:00:00-01:30:00'
-                                    WHEN TIME(fecha_reservacion) BETWEEN '01:30:00' AND '01:59:00' THEN '01:30:00-02:00:00'
-                                 
-                                    WHEN TIME(fecha_reservacion) BETWEEN '02:00:00' AND '02:29:00' THEN '02:00:00-02:30:00'
-                                    WHEN TIME(fecha_reservacion) BETWEEN '02:30:00' AND '02:59:00' THEN '02:30:00-03:00:00'
-                                  
-                                    WHEN TIME(fecha_reservacion) BETWEEN '03:00:00' AND '03:29:00' THEN '03:00:00-03:30:00'
-                                    WHEN TIME(fecha_reservacion) BETWEEN '03:30:00' AND '03:59:00' THEN '03:30:00-04:00:00'
-                                  
-                                    WHEN TIME(fecha_reservacion) BETWEEN '04:00:00' AND '04:29:00' THEN '04:00:00-04:30:00'
-                                    WHEN TIME(fecha_reservacion) BETWEEN '04:30:00' AND '04:59:00' THEN '04:30:00-05:00:00'
-                                
-                                    WHEN TIME(fecha_reservacion) BETWEEN '05:00:00' AND '05:29:00' THEN '05:00:00-05:30:00'
-                                    WHEN TIME(fecha_reservacion) BETWEEN '05:30:00' AND '05:59:00' THEN '05:30:00-06:00:00'
-                                 
-                                    WHEN TIME(fecha_reservacion) BETWEEN '06:00:00' AND '06:29:00' THEN '06:00:00-06:30:00'
-                                    WHEN TIME(fecha_reservacion) BETWEEN '06:30:00' AND '06:59:00' THEN '06:30:00-07:00:00'
-                              
-                                    WHEN TIME(fecha_reservacion) BETWEEN '07:00:00' AND '07:29:00' THEN '07:00:00-07:30:00'
-                                    WHEN TIME(fecha_reservacion) BETWEEN '07:30:00' AND '07:59:00' THEN '07:30:00-08:00:00'
-                      
-                                    WHEN TIME(fecha_reservacion) BETWEEN '08:00:00' AND '08:29:00' THEN '08:00:00-08:30:00'
-                                    WHEN TIME(fecha_reservacion) BETWEEN '08:30:00' AND '08:59:00' THEN '08:30:00-09:00:00'
-                      
-                                    WHEN TIME(fecha_reservacion) BETWEEN '09:00:00' AND '09:29:00' THEN '09:00:00-09:30:00'
-                                    WHEN TIME(fecha_reservacion) BETWEEN '09:30:00' AND '09:59:00' THEN '09:30:00-10:00:00'
-                      
-                                    WHEN TIME(fecha_reservacion) BETWEEN '10:00:00' AND '10:29:00' THEN '10:00:00-10:30:00'
-                                    WHEN TIME(fecha_reservacion) BETWEEN '10:30:00' AND '10:59:00' THEN '10:30:00-11:00:00' 
-                      
-                                    WHEN TIME(fecha_reservacion) BETWEEN '11:00:00' AND '11:29:00' THEN '11:00:00-11:30:00'
-                                    WHEN TIME(fecha_reservacion) BETWEEN '00:30:00' AND '11:59:00' THEN '11:30:00-12:00:00'
-                     
-                                    WHEN TIME(fecha_reservacion) BETWEEN '12:00:00' AND '12:29:00' THEN '12:00:00-12:30:00'
-                                    WHEN TIME(fecha_reservacion) BETWEEN '12:30:00' AND '12:59:00' THEN '12:30:00-13:00:00'
-                     
-                                    WHEN TIME(fecha_reservacion) BETWEEN '13:00:00' AND '13:29:00' THEN '13:00:00-13:30:00'
-                                    WHEN TIME(fecha_reservacion) BETWEEN '13:30:00' AND '13:59:00' THEN '13:30:00-14:00:00'
-                      
-                                    WHEN TIME(fecha_reservacion) BETWEEN '14:00:00' AND '14:29:00' THEN '14:00:00-14:30:00'
-                                    WHEN TIME(fecha_reservacion) BETWEEN '14:30:00' AND '14:59:00' THEN '14:30:00-15:00:00'
-                      
-                                    WHEN TIME(fecha_reservacion) BETWEEN '15:00:00' AND '15:29:00' THEN '15:00:00-15:30:00'
-                                    WHEN TIME(fecha_reservacion) BETWEEN '15:30:00' AND '15:59:00' THEN '15:30:00-16:00:00'
-                    
-                                    WHEN TIME(fecha_reservacion) BETWEEN '16:00:00' AND '16:29:00' THEN '16:00:00-16:30:00'
-                                    WHEN TIME(fecha_reservacion) BETWEEN '16:30:00' AND '16:59:00' THEN '16:30:00-17:00:00'
-                     
-                                    WHEN TIME(fecha_reservacion) BETWEEN '17:00:00' AND '17:29:00' THEN '17:00:00-17:30:00'
-                                    WHEN TIME(fecha_reservacion) BETWEEN '17:30:00' AND '17:59:00' THEN '17:30:00-18:00:00'
-                      
-                                    WHEN TIME(fecha_reservacion) BETWEEN '18:00:00' AND '18:29:00' THEN '18:00:00-18:30:00'
-                                    WHEN TIME(fecha_reservacion) BETWEEN '18:30:00' AND '18:59:00' THEN '18:30:00-19:00:00'
-                      
-                                    WHEN TIME(fecha_reservacion) BETWEEN '19:00:00' AND '19:29:00' THEN '19:00:00-19:30:00'
-                                    WHEN TIME(fecha_reservacion) BETWEEN '19:30:00' AND '19:59:00' THEN '19:30:00-20:00:00'
-                      
-                                    WHEN TIME(fecha_reservacion) BETWEEN '20:00:00' AND '20:29:00' THEN '20:00:00-20:30:00'
-                                    WHEN TIME(fecha_reservacion) BETWEEN '20:30:00' AND '20:59:00' THEN '20:30:00-21:00:00'
-                      
-                                    WHEN TIME(fecha_reservacion) BETWEEN '21:00:00' AND '21:29:00' THEN '21:00:00-21:30:00'
-                                    WHEN TIME(fecha_reservacion) BETWEEN '21:30:00' AND '21:59:00' THEN '21:30:00-22:00:00'
-                      
-                                    WHEN TIME(fecha_reservacion) BETWEEN '22:00:00' AND '22:29:00' THEN '22:00:00-22:30:00'
-                                    WHEN TIME(fecha_reservacion) BETWEEN '22:30:00' AND '22:59:00' THEN '22:30:00-23:00:00'
-                     
-                                    WHEN TIME(fecha_reservacion) BETWEEN '23:00:00' AND '23:29:00' THEN '23:00:00-23:30:00'
-                                    WHEN TIME(fecha_reservacion) BETWEEN '23:30:00' AND '23:59:00' THEN '23:30:00-00:00:00'
-                                END) 
+                                (
+									CASE
+									WHEN MINUTE(TIME(fecha_reservacion)) BETWEEN  0 AND 29 THEN
+										CONCAT(LPAD(HOUR(TIME(fecha_reservacion)),2,'0'), ':00:00-', LPAD(HOUR(TIME(fecha_reservacion)),2,'0'), ':30:00')
+									ELSE
+										CONCAT(LPAD(HOUR(TIME(fecha_reservacion)),2,'0'), ':30:00-', LPAD(HOUR(TIME(fecha_reservacion))+1,2,'0'), ':00:00')
+									END
+								) 
                             as intervalo, count(*) FROM `cita` WHERE usuario_id=".$request->request->get('user')
                             ." AND fecha_reservacion BETWEEN '".$request->request->get('cita')["fechaReservacion"]." 00:00:00' AND '"
                             .$request->request->get('cita')["fechaReservacion"]." 23:59:59' GROUP BY intervalo) as r GROUP BY r.intervalo) 
