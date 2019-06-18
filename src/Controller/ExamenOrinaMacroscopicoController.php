035121dbb597fc4ea9676b5f9f455176a89a251e
Mego1596 <maurigranados1596@gmail.com>
2019-06-17 23:35:50 -0600

    ER Examen Orina Macroscopico INDEX

--- src/Controller/ExamenOrinaMacroscopicoController.php
+++ src/Controller/ExamenOrinaMacroscopicoController.php
@@ -134,24 +134,21 @@ class ExamenOrinaMacroscopicoController extends AbstractController
     {
         if($AuthUser->getUser()->getRol()->getNombreRol() != 'ROLE_SA'){
             if($AuthUser->getUser()->getClinica()->getId() == $examen_solicitado->getCita()->getExpediente()->getUsuario()->getClinica()->getId() && $examenOrinaMacroscopico->getExamenSolicitado()->getId() == $examen_solicitado->getId()){
-                if($examen_solicitado->getCita()->getExpediente()->getHabilitado()){
-                    return $this->render('examen_orina_macroscopico/show.html.twig', [
-                        'examen_orina_macroscopico' => $examenOrinaMacroscopico,
-                        'examen_solicitado' => $examen_solicitado,
-                    ]);
-                }else{
-                    $this->addFlash('fail','Este paciente no está habilitado, para poder hacer uso de el consulte con su superior para habilitar el paciente');
-                    return $this->redirectToRoute('home');
-                }
             }else{
                 $this->addFlash('fail','Error, este registro puede que no exista o no le pertenece');
                 return $this->redirectToRoute('home');
             }  
         }
-        return $this->render('examen_orina_macroscopico/show.html.twig', [
-            'examen_orina_macroscopico' => $examenOrinaMacroscopico,
-            'examen_solicitado' => $examen_solicitado,
-        ]);
+        
+        if($examen_solicitado->getCita()->getExpediente()->getHabilitado()){
+            return $this->render('examen_orina_macroscopico/show.html.twig', [
+                'examen_orina_macroscopico' => $examenOrinaMacroscopico,
+                'examen_solicitado' => $examen_solicitado,
+            ]);
+        }else{
+            $this->addFlash('fail','Este paciente no está habilitado, para poder hacer uso de el consulte con su superior para habilitar el paciente');
+            return $this->redirectToRoute('home');
+        }
     }
 
     /**
