47feea46eba2f5e0f2274d7b105e7522760fd002
Mego1596 <maurigranados1596@gmail.com>
2019-06-17 22:06:30 -0600

    ER Examen Heces Microscopico EDIT

--- src/Controller/ExamenHecesMicroscopicoController.php
+++ src/Controller/ExamenHecesMicroscopicoController.php
@@ -162,35 +162,13 @@ class ExamenHecesMicroscopicoController extends AbstractController
 
         if($AuthUser->getUser()->getRol()->getNombreRol() != 'ROLE_SA'){
             if($AuthUser->getUser()->getClinica()->getId() == $examen_solicitado->getCita()->getExpediente()->getUsuario()->getClinica()->getId() && $examenHecesMicroscopico->getExamenSolicitado()->getId() == $examen_solicitado->getId()){
-                if($examen_solicitado->getCita()->getExpediente()->getHabilitado()){
-                    $editar = true;
-                    $form = $this->createForm(ExamenHecesMicroscopicoType::class, $examenHecesMicroscopico);
-                    $form->handleRequest($request);
-
-                    if ($form->isSubmitted() && $form->isValid()) {
-                        $this->getDoctrine()->getManager()->flush();
-                        $this->addFlash('success', 'Examen modificado con éxito');
-                        return $this->redirectToRoute('examen_heces_microscopico_index', [
-                            'id' => $examenHecesMicroscopico->getId(),
-                            'examen_solicitado' => $examen_solicitado->getId(),
-                        ]);
-                    }
-                    return $this->render('examen_heces_microscopico/edit.html.twig', [
-                        'examen_heces_microscopico' => $examenHecesMicroscopico,
-                        'examen_solicitado' => $examen_solicitado,
-                        'editar'            => $editar,
-                        'form' => $form->createView(),
-                    ]);
-                }else{
-                    $this->addFlash('fail','Este paciente no está habilitado, para poder hacer uso de el consulte con su superior para habilitar el paciente');
-                    return $this->redirectToRoute('home');
-                }
+                
             }else{
                 $this->addFlash('fail','Error, este registro puede que no exista o no le pertenece');
                 return $this->redirectToRoute('home');
             }  
         }
-        
+
         if($examen_solicitado->getCita()->getExpediente()->getHabilitado()){
             $editar = true;
             $form = $this->createForm(ExamenHecesMicroscopicoType::class, $examenHecesMicroscopico);
