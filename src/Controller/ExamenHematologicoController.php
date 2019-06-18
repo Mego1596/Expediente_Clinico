488f3151e6344f55032ea4b65447636251c97c10
Mego1596 <maurigranados1596@gmail.com>
2019-06-17 22:30:26 -0600

    ER Examen Hematologico SHOW

--- src/Controller/ExamenHematologicoController.php
+++ src/Controller/ExamenHematologicoController.php
@@ -118,24 +118,20 @@ class ExamenHematologicoController extends AbstractController
     {
         if($AuthUser->getUser()->getRol()->getNombreRol() != 'ROLE_SA'){
             if($AuthUser->getUser()->getClinica()->getId() == $examen_solicitado->getCita()->getExpediente()->getUsuario()->getClinica()->getId() && $examenHematologico->getExamenSolicitado()->getId() == $examen_solicitado->getId()){
-                if($examen_solicitado->getCita()->getExpediente()->getHabilitado()){
-                    return $this->render('examen_hematologico/show.html.twig', [
-                        'examen_hematologico' => $examenHematologico,
-                        'examen_solicitado' => $examen_solicitado,
-                    ]);
-                }else{
-                    $this->addFlash('fail','Este paciente no está habilitado, para poder hacer uso de el consulte con su superior para habilitar el paciente');
-                    return $this->redirectToRoute('home');
-                }
             }else{
                 $this->addFlash('fail','Error, este registro puede que no exista o no le pertenece');
                 return $this->redirectToRoute('home');
             }  
         }
-        return $this->render('examen_hematologico/show.html.twig', [
-            'examen_hematologico' => $examenHematologico,
-            'examen_solicitado' => $examen_solicitado,
-        ]);
+        if($examen_solicitado->getCita()->getExpediente()->getHabilitado()){
+            return $this->render('examen_hematologico/show.html.twig', [
+                'examen_hematologico' => $examenHematologico,
+                'examen_solicitado' => $examen_solicitado,
+            ]);
+        }else{
+            $this->addFlash('fail','Este paciente no está habilitado, para poder hacer uso de el consulte con su superior para habilitar el paciente');
+            return $this->redirectToRoute('home');
+        }
     }
 
     /**
