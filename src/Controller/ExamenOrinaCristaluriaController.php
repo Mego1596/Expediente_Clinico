ea40655fbc438bb2cd49b5a30d742105ccf503b7
Mego1596 <maurigranados1596@gmail.com>
2019-06-17 23:04:28 -0600

    ER Examen Orina Cristaluria EDIT

--- src/Controller/ExamenOrinaCristaluriaController.php
+++ src/Controller/ExamenOrinaCristaluriaController.php
@@ -174,35 +174,12 @@ class ExamenOrinaCristaluriaController extends AbstractController
 
         if($AuthUser->getUser()->getRol()->getNombreRol() != 'ROLE_SA'){
             if($AuthUser->getUser()->getClinica()->getId() == $examen_solicitado->getCita()->getExpediente()->getUsuario()->getClinica()->getId() && $examenOrinaCristaluria->getExamenSolicitado()->getId() == $examen_solicitado->getId()){
-                if($examen_solicitado->getCita()->getExpediente()->getHabilitado()){
-                    $editar = true;
-                    $form = $this->createForm(examenOrinaCristaluriaType::class, $examenOrinaCristaluria);
-                    $form->handleRequest($request);
-
-                    if ($form->isSubmitted() && $form->isValid()) {
-                        $this->getDoctrine()->getManager()->flush();
-                        $this->addFlash('success', 'Examen modificado con éxito');
-                        return $this->redirectToRoute('examen_orina_cristaluria_index', [
-                            'id' => $examenOrinaCristaluria->getId(),
-                            'examen_solicitado' => $examen_solicitado->getId(),
-                        ]);
-                    }
-                    return $this->render('examen_orina_cristaluria/edit.html.twig', [
-                        'examen_orina_cristaluria' => $examenOrinaCristaluria,
-                        'examen_solicitado' => $examen_solicitado,
-                        'editar'            => $editar,
-                        'form' => $form->createView(),
-                    ]);
-                }else{
-                    $this->addFlash('fail','Este paciente no está habilitado, para poder hacer uso de el consulte con su superior para habilitar el paciente');
-                    return $this->redirectToRoute('home');
-                }
             }else{
                 $this->addFlash('fail','Error, este registro puede que no exista o no le pertenece');
                 return $this->redirectToRoute('home');
             }  
         }
-        
+
         if($examen_solicitado->getCita()->getExpediente()->getHabilitado()){
             $editar = true;
             $form = $this->createForm(examenOrinaCristaluriaType::class, $examenOrinaCristaluria);
