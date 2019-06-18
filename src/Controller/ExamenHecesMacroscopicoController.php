6501acd3374711737ff3421b0363e55554e031b3
Mego1596 <maurigranados1596@gmail.com>
2019-06-17 21:31:29 -0600

    ER Examen Heces Macroscopico DELETE

--- src/Controller/ExamenHecesMacroscopicoController.php
+++ src/Controller/ExamenHecesMacroscopicoController.php
@@ -208,18 +208,7 @@ class ExamenHecesMacroscopicoController extends AbstractController
     {
         if($AuthUser->getUser()->getRol()->getNombreRol() != 'ROLE_SA'){
             if($AuthUser->getUser()->getClinica()->getId() == $examen_solicitado->getCita()->getExpediente()->getUsuario()->getClinica()->getId()){
-                if($examen_solicitado->getCita()->getExpediente()->getHabilitado()){
-                    if ($this->isCsrfTokenValid('delete'.$examenHecesMacroscopico->getId(), $request->request->get('_token'))) {
-                        $entityManager = $this->getDoctrine()->getManager();
-                        $entityManager->remove($examenHecesMacroscopico);
-                        $entityManager->flush();
-                    }
-                    $this->addFlash('success', 'Examen eliminado con éxito');
-                    return $this->redirectToRoute('examen_heces_macroscopico_index',['examen_solicitado' => $examen_solicitado->getId()]);
-                }else{
-                    $this->addFlash('fail','Este paciente no está habilitado, para poder hacer uso de el consulte con su superior para habilitar el paciente');
-                    return $this->redirectToRoute('home');
-                }
+                
             }else{
                 $this->addFlash('fail','Error, este registro puede que no exista o no le pertenece');
                 return $this->redirectToRoute('home');
